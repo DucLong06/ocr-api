3593a1455e0ce3b03a50fd6636cfaa004eef4ba1
DucLong06 <hoangduclongg@gmail.com>
2022-05-06 08:49:26 +0700

    fix app fe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { createWorker } from 'tesseract.js';
-import { FilePond, registerPlugin } from 'react-filepond';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
 import 'filepond/dist/filepond.min.css';
+import React from 'react';
+import { FilePond, registerPlugin } from 'react-filepond';
+import { createWorker } from 'tesseract.js';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 registerPlugin(FilePondPluginImagePreview);
 
