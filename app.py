664adefebf128a5d3142209121f495510fdd3d55
DucLong06 <hoangduclongg@gmail.com>
2022-10-24 08:48:08 +0700

    add ping

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ app = Flask(__name__)
 CORS(app)
 
 # Load model
-# PATH_TO_MODEL = my_yolo.load_model(my_env.PATH_TO_MODEL)
 MODEL_CV = my_yolo.load_model(my_env.PATH_TO_MODEL_CV)
 MODEL_DETECT_CI = my_yolo.load_model(my_env.PATH_TO_MODEL_CI)
 # Loger
@@ -26,9 +25,11 @@ logger = my_logger.Logger("LOG", my_env.LOG)
 def _call_my_yolo(model, path_to_save):
     return my_yolo.predict_model(model, path_to_save)
 
+
 @app.route("/", methods=["GET"])
 def ping():
-    return "msg: ok ok"
+    return {"msg": "Server is OK"}
+
 
 @app.route("/detect-ci", methods=["POST"])
 def detect_text():
@@ -41,9 +42,9 @@ def detect_text():
             )
             image.save(path_to_save)
             logger.info("Save file: %s" % path_to_save)
-            output, _ = _call_my_yolo(MODEL_DETECT_CI, path_to_save)
+            imagebase64, area = _call_my_yolo(MODEL_DETECT_CI, path_to_save)
             logger.info("Done processing")
-        return output
+        return {"Area": area, "ImageBase64": str(imagebase64)}
     except Exception as e:
         logger.error("Error processing image: %s" % str(e))
     return "Upload file to detect"
@@ -60,13 +61,31 @@ def detect_cv():
             )
             image.save(path_to_save)
             logger.info("Save file: %s" % path_to_save)
-            _, output = _call_my_yolo(MODEL_CV, path_to_save)
+            imagebase64, area = _call_my_yolo(MODEL_CV, path_to_save)
             logger.info("Done processing")
-        return output
+        return {"area": area, "imagebase64": imagebase64}
     except Exception as e:
         logger.error("Error processing image: %s" % str(e))
     return "Upload file to detect"
 
+
+@app.route("/img2text", methods=["POST"])
+def recognize():
+    image = request.files["file"]
+    logger.info("Improcessing image: %s" % str(image.filename))
+    try:
+        if image:
+            path_to_save = os.path.join(
+                my_env.UPLOAD_FOLDER, str(uuid.uuid4()) + image.filename
+            )
+            image.save(path_to_save)
+            logger.info("Save file: %s" % path_to_save)
+            imagebase64, area = _call_my_yolo(MODEL_CV, path_to_save)
+            logger.info("Done processing")
+        return {"area": area, "imagebase64": imagebase64}
+    except Exception as e:
+        logger.error("Error processing image: %s" % str(e))
+    return "Upload file to detect"
 
 # Start Backend
 if __name__ == "__main__":
